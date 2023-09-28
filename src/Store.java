3e8251a586f5db1aa30c726df44722f388caf075
AnilEnsari <anilensari@gmail.com>
2023-09-28 14:10:40 +0300

    all done

--- src/Store.java
+++ src/Store.java
@@ -1,4 +1,6 @@
 
+import MonsterForSimpleRPGGame.Monster;
+import MonsterForSimpleRPGGame.Werewolf;
 import ProductForSale.ProductForSale;
 import ProductForSale.Bread;
 import ProductForSale.Chocolate;
@@ -11,8 +13,16 @@ public class Store {
         ProductForSale chocolate = new Chocolate("desert", 12, "milky and hazelnut", false);
         ProductForSale coke = new Coke("drink", 10, "zero sugar", true);
         ProductForSale [] products = {bread,chocolate,coke};
+        Monster werewolf = new Werewolf("Werewolf",100,15) ;
+        System.out.println("Werewolf is bleeding. Damage is: "+ werewolf.bleed());
+        System.out.println("Werewolf is poisoning. Damage is: "+ werewolf.poison());
+        System.out.println("Werewolf is attacking. Damage is: "+ werewolf.attack());
+
+
+        System.out.println("***************************");
 
         listProducts(products);
+
     }
 
     public static void listProducts(ProductForSale[] products) {
