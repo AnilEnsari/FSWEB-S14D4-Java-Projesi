523e5279e2cf75a258ef1265f14e55b00a43eda9
AnilEnsari <anilensari@gmail.com>
2023-09-28 13:37:37 +0300

    first step is done

--- src/Store.java
+++ src/Store.java
@@ -1,10 +1,27 @@
+
+import ProductForSale.ProductForSale;
+import ProductForSale.Bread;
+import ProductForSale.Chocolate;
+import ProductForSale.Coke;
+
+
 public class Store {
     public static void main(String[] args) {
+        ProductForSale bread = new Bread("carbonhydrate", 8, "less calories", true);
+        ProductForSale chocolate = new Chocolate("desert", 12, "milky and hazelnut", false);
+        ProductForSale coke = new Coke("drink", 10, "zero sugar", true);
+        ProductForSale [] products = {bread,chocolate,coke};
 
-        listProducts();
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
+        for (ProductForSale product : products) {
+
+            product.showDetails();
+        }
 
     }
+
+
 }
